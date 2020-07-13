4382e172dcadaa1e8ba29ce71730ec6e9866e342
EdTheDev <edward@delaporte.us>
2020-07-12 20:31:18 -0500

    spelling, perms

--- deploy.ps1
+++ deploy.ps1
@@ -1,11 +1,13 @@
 
 
-$gameDir = "/opt/retropi/ports/joytest"
+$gameDir = "/opt/retropie/ports/joytest"
 $scriptDir = "/home/pi/RetroPie/roms/ports"
 $script = "/home/pi/RetroPie/roms/ports/joytest.sh"
 
-ssh pi@tmntcade -C "mkdir $gameDir"
-scp ./joytest/joytest.sh pi@tmntcade:"$gameDir"
+ssh pi@tmntcade -C "sudo mkdir -p $gameDir"
+ssh pi@tmntcade -C "sudo chown pi $gameDir"
+scp ./joytest/joytest.sh pi@tmntcade:"$scriptDir"
+scp ./joytest/* pi@tmntcade:"$gameDir"
 ssh pi@tmntcade -C "chmod +x $script"
 ssh pi@tmntcade -C "ls -al $gameDir"
 ssh pi@tmntcade -C "ls -al $scriptDir"
