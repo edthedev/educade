0a2d45b8258005824d6a3a72305dd2721e696d00
EdTheDev <edward@delaporte.us>
2020-07-12 20:16:07 -0500

    Deploy key correctly

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,6 @@
 
-
 pip install pygame
 
 ssh-keygen
 ssh pi@tmntcade -C 'mkdir ~/.ssh'
-cd ~;scp .ssh/id_rsa.pub pi@tmntcade:~/.ssh/id_rsa.pub
+cd ~;scp .ssh/id_rsa.pub pi@tmntcade:~/.ssh/authorized_keys
