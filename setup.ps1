0a916a3c0b507442c7a6bc54f0f48b789d017fd0
EdTheDev <edward@delaporte.us>
2020-07-12 21:14:31 -0500

    dos2unix

--- setup.ps1
+++ setup.ps1
@@ -5,3 +5,4 @@ ssh pi@tmntcade -C 'mkdir ~/.ssh'
 cd ~;scp .ssh/id_rsa.pub pi@tmntcade:~/.ssh/authorized_keys
 
 ssh pi@tmntcade -C 'sudo apt-get install -y python-pygame'
+ssh pi@tmntcade -C 'sudo apt-get install -y dos2unix'
