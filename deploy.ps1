e882d85d25c996e4802d214be14922b53320ade6
EdTheDev <edward@delaporte.us>
2020-07-17 21:46:35 -0500

    simplified generic deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,19 +1,25 @@
 
 
 # $gameName = "joytest"
-$gameName = "inv4ders"
+# $gameName = "inv4ders"
 # $gameName = "bouncetest"
 
-$gameDir = "/opt/retropie/ports/$gameName"
-$scriptDir = "/home/pi/RetroPie/roms/ports"
-$script = "/home/pi/RetroPie/roms/ports/$gameName.sh"
 
-ssh pi@tmntcade -C "sudo mkdir -p $gameDir"
-ssh pi@tmntcade -C "sudo chown pi $gameDir"
-scp "./$gameName/$gameName.sh" pi@tmntcade:"$scriptDir"
-scp "./$gameName/$gameName.py" pi@tmntcade:"$gameDir"
-ssh pi@tmntcade -C "chmod +x $script"
-ssh pi@tmntcade -C "dos2unix $script"
-ssh pi@tmntcade -C "dos2unix $gameDir/*"
-ssh pi@tmntcade -C "ls -al $gameDir"
-ssh pi@tmntcade -C "ls -al $scriptDir"
+Write-Host "Deploying Launcher Scripts"
+scp "./scripts/*.sh" pi@tmntcade:"$scriptDir"
+ssh pi@tmntcade -C "chmod +x /home/pi/RetroPie/roms/ports/*.sh"
+ssh pi@tmntcade -C "dos2unix /home/pi/RetroPie/roms/ports/*.sh"
+
+Write-Host "Deploying Game Files"
+
+$games = "joytest","inv4ders"
+
+foreach ($gameName in $games) {
+    $gameDir = "/opt/retropie/ports/$gameName"
+    ssh pi@tmntcade -C "sudo mkdir -p $gameDir"
+    ssh pi@tmntcade -C "sudo chown pi $gameDir"
+    scp "./$gameName/*" pi@tmntcade:"$gameDir"
+    ssh pi@tmntcade -C "dos2unix $gameDir/*"
+    ssh pi@tmntcade -C "ls -al $gameDir"
+}
+ssh pi@tmntcade -C "ls -al /home/pi/RetroPie/roms/ports"
