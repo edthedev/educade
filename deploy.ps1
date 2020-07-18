747e92535bebe5d1a2c2541c49a3395dcf6ade43
EdTheDev <edward@delaporte.us>
2020-07-17 21:50:07 -0500

    Better output

--- deploy.ps1
+++ deploy.ps1
@@ -18,8 +18,10 @@ foreach ($gameName in $games) {
     $gameDir = "/opt/retropie/ports/$gameName"
     ssh pi@tmntcade -C "sudo mkdir -p $gameDir"
     ssh pi@tmntcade -C "sudo chown pi $gameDir"
-    scp "./$gameName/*" pi@tmntcade:"$gameDir"
+    scp -r "./$gameName/*" pi@tmntcade:"$gameDir"
     ssh pi@tmntcade -C "dos2unix $gameDir/*"
-    ssh pi@tmntcade -C "ls -al $gameDir"
+    Write-Host "List of $gameName Files"
+    ssh pi@tmntcade -C "ls -alR $gameDir"
 }
+Write-Host "List of Launcher Scripts"
 ssh pi@tmntcade -C "ls -al /home/pi/RetroPie/roms/ports"
