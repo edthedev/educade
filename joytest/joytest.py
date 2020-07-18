40090b05d995ba3d3e151071bcbda7ac6bbab60b
EdTheDev <edward@delaporte.us>
2020-07-17 21:25:56 -0500

    Cleanup and update joytest

--- joytest/joytest.py
+++ joytest/joytest.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python3
-"""
-Sample Python/Pygame Programs
-Simpson College Computer Science
-http://programarcadegames.com/
-http://simpson.edu/computer-science/
- 
-Show everything we can pull off the joystick
-"""
+"""Four Player invaders clone."""
+
 import pygame
- 
+
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
- 
- 
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+PURPLE = (128, 0, 128)
+YELLOW = (255, 255, 0)
+
+# Joystick X / Y Axis
+JOY_X = 0
+JOY_Y = 1
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# Initialize the joysticks
+pygame.init()
+pygame.joystick.init()
+size = [700, 500]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Inv4ders")
+pygame.mouse.set_visible(False)
+
 class TextPrint(object):
     """
     This is a simple class that will help us print to the screen
@@ -46,117 +59,183 @@ class TextPrint(object):
     def unindent(self):
         """ Unindent the next line of text """
         self.x_pos -= 10
- 
-pygame.init()
- 
-# Set the width and height of the screen [width,height]
-size = [500, 700]
-screen = pygame.display.set_mode(size)
- 
-pygame.display.set_caption("My Game")
- 
-# Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
- 
-# Initialize the joysticks
-pygame.joystick.init()
- 
-# Get ready to print
+
 textPrint = TextPrint()
- 
+
+class ControlSet():
+    """A controller mapping."""
+
+    def __init__(self, up=pygame.K_UP, down=pygame.K_DOWN,
+                 left=pygame.K_LEFT, right=pygame.K_RIGHT):
+        self.up_key = up
+        self.down_key = down
+        self.left_key = left
+        self.right_key = right
+
+
+class Player():
+    """A game player."""
+    controls = [ControlSet()]
+    pos_x = 100
+    pos_y = 100
+    size_x = 50
+    size_y = 50
+    move_amt = 5
+    color = RED
+
+    def __init__(self, color, controls):
+        """Make a new player.
+
+        Assign unique color and controls."""
+        self.controls = controls
+        self.color = color
+
+    def control(self, keys=None, joystick=None):
+        """Look for signals accepted by this player, and apply them."""
+        if keys:
+            self._key_control(keys)
+        if joystick:
+            self._joy_control(joystick)
+
+    def _joy_control(self, joystick):
+        """Apply joystick controls.
+
+        Only pass in the joystick you want to have accepted.
+        """
+        if joystick.get_axis(JOY_Y) >= .8:
+            textPrint.print(screen, "Joystick DOWN: {}".format(joystick.get_axis(JOY_Y)))
+            self._down()
+        if joystick.get_axis(JOY_Y) <= -.8:
+            textPrint.print(screen, "Joystick UP: {}".format(joystick.get_axis(JOY_Y)))
+            self._up()
+        if joystick.get_axis(JOY_X) >= .8:
+            textPrint.print(screen, "Joystick RIGHT: {}".format(joystick.get_axis(JOY_X)))
+            self._right()
+        if joystick.get_axis(JOY_X) <= -.8:
+            textPrint.print(screen, "Joystick LEFT: {}".format(joystick.get_axis(JOY_X)))
+            self._left()
+
+    def _up(self):
+        """Move self up."""
+        self.pos_y -= self.move_amt
+
+    def _down(self):
+        """Move self down."""
+        self.pos_y += self.move_amt
+
+    def _left(self):
+        """Move self left."""
+        self.pos_x -= self.move_amt
+
+    def _right(self):
+        """Move self right."""
+        self.pos_x += self.move_amt
+
+    def _key_control(self, keys):
+        """Apply keyboard controls - as accepted by this player."""
+        for control_set in self.controls:
+            if keys[control_set.up_key]:
+                self._up()
+            if keys[control_set.down_key]:
+                self._down()
+            if keys[control_set.left_key]:
+                self._left()
+            if keys[control_set.right_key]:
+                self._right()
+
+    def draw(self):
+        """Draw the player."""
+        pygame.draw.rect(screen, WHITE,
+                         [self.pos_x, self.pos_y, self.size_x, self.size_y])
+        pygame.draw.rect(screen, self.color,
+                         [self.pos_x + 10, self.pos_y + 10, self.size_x - 10, self.size_y - 10])
+
+
+class invaders():
+    """Game loggic goes here."""
+    done = False
+
+    player1 = Player(color=YELLOW,
+                     controls=[ControlSet(), ControlSet(up=pygame.K_w, down=pygame.K_s,
+                                                        left=pygame.K_a, right=pygame.K_d)])
+    player2 = Player(color=RED,
+                     controls=[ControlSet(up=pygame.K_j, down=pygame.K_k,
+                                          left=pygame.K_h, right=pygame.K_l)])
+    player3 = Player(color=BLUE,
+                     controls=[ControlSet(up=pygame.K_3, down=pygame.K_2,
+                                          left=pygame.K_1, right=pygame.K_4)])
+    player4 = Player(color=PURPLE,
+                     controls=[ControlSet(up=pygame.K_7, down=pygame.K_6,
+                                          left=pygame.K_5, right=pygame.K_8)])
+
+    def controls(self):
+        """Check for control inputs."""
+
+        # Keyboard controls - for testing without joysticks
+
+        # events = pygame.event.get()
+
+        keys = pygame.key.get_pressed()
+        self.player1.control(keys=keys)
+        self.player2.control(keys=keys)
+        self.player3.control(keys=keys)
+        self.player4.control(keys=keys)
+
+        # Get count of joysticks
+        joystick_count = pygame.joystick.get_count()
+
+        # For each joystick:
+        for i in range(joystick_count):
+            joystick = pygame.joystick.Joystick(i)
+            joystick.init()
+
+            # Exit like RetroArch - any joystick
+            button7 = joystick.get_button(7)
+            button8 = joystick.get_button(8)
+            if button7 == button8 == 1:
+                self.done = True
+
+            if i == 0:
+                self.player1.control(joystick=joystick)
+            if i == 1:
+                self.player2.control(joystick=joystick)
+            if i == 2:
+                self.player3.control(joystick=joystick)
+            if i == 3:
+                self.player4.control(joystick=joystick)
+
+    def logic(self):
+        """Calculate game logic."""
+        # --- Event Processing
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.done = True
+
+    def draw(self):
+        """Update the screen."""
+        # --- Drawing
+        # Set the screen background
+
+        self.player1.draw()
+        self.player2.draw()
+        self.player3.draw()
+        self.player4.draw()
+
+        # Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+        textPrint.reset()
+        screen.fill(WHITE)
+        joystick_count = pygame.joystick.get_count()
+        textPrint.print(screen, "Number of joysticks: {}".format(joystick_count))
+
 # -------- Main Program Loop -----------
-while not done:
-    # EVENT PROCESSING STEP
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-
-
- 
-        # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN
-        # JOYBUTTONUP JOYHATMOTION
-        if event.type == pygame.JOYBUTTONDOWN:
-            print("Joystick button pressed.")
-        if event.type == pygame.JOYBUTTONUP:
-            print("Joystick button released.")
-        
-
- 
-    # DRAWING STEP
-    # First, clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
-    screen.fill(WHITE)
-    textPrint.reset()
- 
-    # Get count of joysticks
-    joystick_count = pygame.joystick.get_count()
- 
-    textPrint.print(screen, "Number of joysticks: {}".format(joystick_count))
-    textPrint.indent()
- 
-    # For each joystick:
-    for i in range(joystick_count):
-        joystick = pygame.joystick.Joystick(i)
-        joystick.init()
-
-        # Exit like RetroArch
-        button7 = joystick.get_button(7)
-        button8 = joystick.get_button(8)
-        if button7 == button8 == 1:
-            done = True
- 
-        textPrint.print(screen, "Joystick {}".format(i))
-        textPrint.indent()
- 
-        # Get the name from the OS for the controller/joystick
-        name = joystick.get_name()
-        textPrint.print(screen, "Joystick name: {}".format(name))
- 
-        # Usually axis run in pairs, up/down for one, and left/right for
-        # the other.
-        axes = joystick.get_numaxes()
-        textPrint.print(screen, "Number of axes: {}".format(axes))
-        textPrint.indent()
- 
-        for i in range(axes):
-            axis = joystick.get_axis(i)
-            textPrint.print(screen, "Axis {} value: {:>6.3f}".format(i, axis))
-        textPrint.unindent()
- 
-        buttons = joystick.get_numbuttons()
-        textPrint.print(screen, "Number of buttons: {}".format(buttons))
-        textPrint.indent()
- 
-        for i in range(buttons):
-            button = joystick.get_button(i)
-            textPrint.print(screen, "Button {:>2} value: {}".format(i, button))
-        textPrint.unindent()
- 
-        # Hat switch. All or nothing for direction, not like joysticks.
-        # Value comes back in an array.
-        hats = joystick.get_numhats()
-        textPrint.print(screen, "Number of hats: {}".format(hats))
-        textPrint.indent()
- 
-        for i in range(hats):
-            hat = joystick.get_hat(i)
-            textPrint.print(screen, "Hat {} value: {}".format(i, str(hat)))
-        textPrint.unindent()
- 
-        textPrint.unindent()
- 
-    # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
- 
-    # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
+inv = invaders()
+while not inv.done:
+    inv.controls()
+    inv.logic()
+    inv.draw()
     # Limit to 60 frames per second
     clock.tick(60)
- 
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
+
+# Close everything down
 pygame.quit()
