d2ce668d980c931136cce323eddb0d2eefe857cc
EdTheDev <edward@delaporte.us>
2020-09-23 22:01:12 -0500

    missed one

--- setup.ps1
+++ setup.ps1
@@ -5,7 +5,7 @@ pip install pygame
 $hostName = "pi@rampage"
 
 ssh ${hostName} -C 'mkdir ~/.ssh'
-cd ~;scp .ssh/id_rsa.pub $hostName:~/.ssh/authorized_keys
+cd ~;scp .ssh/id_rsa.pub ${hostName}:~/.ssh/authorized_keys
 
 ssh ${hostName} -C 'sudo apt-get install -y python3-pygame python3'
 ssh ${hostName} -C 'sudo apt-get install -y dos2unix'
