aceca8226bfbd79b879aa2235cbe87559e7c6a3b
EdTheDev <edward@delaporte.us>
2020-09-23 22:00:50 -0500

    esaier to switch hostnames

--- deploy.ps1
+++ deploy.ps1
@@ -4,12 +4,15 @@
 # $gameName = "inv4ders"
 # $gameName = "bouncetest"
 
+$hostName = "pi@tmntcade"
+# $hostName = "pi@rampage"
+
 $scriptDir = "/home/pi/RetroPie/roms/ports"
 
 Write-Host "Deploying Launcher Scripts"
-scp "./scripts/*.sh" pi@tmntcade:"$scriptDir"
-ssh pi@tmntcade -C "chmod +x $scriptDir/*.sh"
-ssh pi@tmntcade -C "dos2unix $scriptDir/*.sh"
+scp "./scripts/*.sh" ${hostName}:"$scriptDir"
+ssh ${hostName} -C "chmod +x $scriptDir/*.sh"
+ssh ${hostName} -C "dos2unix $scriptDir/*.sh"
 
 # $games = "joytest","inv4ders","peanut"
 $games = "inv4ders","peanut"
@@ -20,13 +23,13 @@ Write-Host "Deploying Game Files"
 
 foreach ($gameName in $games) {
     $gameDir = "/opt/retropie/ports/$gameName"
-    ssh pi@tmntcade -C "sudo mkdir -p $gameDir"
-    ssh pi@tmntcade -C "sudo chown pi $gameDir"
-    ssh pi@tmntcade -C "rm -rf $gameDir/*"
-    scp -r "./$gameName/*" pi@tmntcade:"$gameDir"
-    ssh pi@tmntcade -C "dos2unix $gameDir/*"
+    ssh ${hostName} -C "sudo mkdir -p $gameDir"
+    ssh ${hostName} -C "sudo chown pi $gameDir"
+    ssh ${hostName} -C "rm -rf $gameDir/*"
+    scp -r "./$gameName/*" ${hostName}:"$gameDir"
+    ssh ${hostName} -C "dos2unix $gameDir/*"
     Write-Host "List of $gameName Files"
-    ssh pi@tmntcade -C "ls -alR $gameDir"
+    ssh ${hostName} -C "ls -alR $gameDir"
 }
 Write-Host "List of Launcher Scripts"
-ssh pi@tmntcade -C "ls -al /home/pi/RetroPie/roms/ports"
+ssh ${hostName} -C "ls -al /home/pi/RetroPie/roms/ports"
--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,11 @@
 
 pip install pygame
 
-ssh pi@tmntcade -C 'mkdir ~/.ssh'
-cd ~;scp .ssh/id_rsa.pub pi@tmntcade:~/.ssh/authorized_keys
+# $hostName = "pi@tmntcade"
+$hostName = "pi@rampage"
 
-ssh pi@tmntcade -C 'sudo apt-get install -y python3-pygame python3'
-ssh pi@tmntcade -C 'sudo apt-get install -y dos2unix'
+ssh ${hostName} -C 'mkdir ~/.ssh'
+cd ~;scp .ssh/id_rsa.pub $hostName:~/.ssh/authorized_keys
+
+ssh ${hostName} -C 'sudo apt-get install -y python3-pygame python3'
+ssh ${hostName} -C 'sudo apt-get install -y dos2unix'
