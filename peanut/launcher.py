000f753688f31580d23ddfb279b012f36336ccf0
EdTheDev <edward@delaporte.us>
2020-09-26 15:38:07 -0500

    Launchers look like launchers!

--- peanut/launcher.py
+++ peanut/launcher.py
@@ -1,10 +1,11 @@
 """Launch a Nutkin!"""
 from dataclasses import dataclass, field
-from typing import List, Set, Dict, Tuple, Optional
+from typing import List
 
 import pygame
 from colors import Colors
 from player import Player
+from image import Images
 
 @dataclass
 class Launcher():
@@ -14,17 +15,34 @@ class Launcher():
     pos_x: int = 50
     pos_y: int = 50
     size_x: int = 50
-    size_y: int = 50
-    players: List[Player]=field(default_factory=list)
+    size_y: int = 80
+    players: List[Player] = field(default_factory=list)
+    img_launcher = pygame.image.load(Images.get_path(r'launcher.png'))
+    img_launched = pygame.image.load(Images.get_path(r'launched.png'))
+    img = img_launcher
+    launching: int = 0
+    size_change = 20
 
     def __post_init__(self):
         """Start with an empty player list."""
         self._players = []
+        self.img_launched = pygame.transform.scale(self.img_launched, 
+                                                   (self.size_x, self.size_y + self.size_change))
+        self.img_launcher = pygame.transform.scale(self.img_launcher, (self.size_x, self.size_y))
+        self.img = self.img_launcher
 
     def draw(self, screen):
         """Draw the launcher."""
-        pygame.draw.rect(screen, Colors.RED,
-                [self.pos_x, self.pos_y, self.size_x, self.size_y])
+        if self.launching > 0:
+            self.img = self.img_launched
+            self.launching -= 1
+            screen.blit(self.img, (self.pos_x, self.pos_y - self.size_change))
+        else:
+            self.img = self.img_launcher
+            screen.blit(self.img, (self.pos_x, self.pos_y))
+
+        #pygame.draw.rect(screen, Colors.RED,
+        #                 [self.pos_x, self.pos_y, self.size_x, self.size_y])
 
     def add_player(self, player):
         """Add a player to the launcher."""
@@ -35,4 +53,5 @@ class Launcher():
         for player in self._players:
             if player != no_launch_player:
                 player.launched = 1
+                self.launching = 8
             self._players = []
